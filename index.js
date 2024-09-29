abf157a05486edbf48f4ae6027286e5c37bef85b
shivprakashh <shivpraksh518@gmail.com>
2024-09-29 15:08:31 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const app = express();
+const http = require("http")
+const path = require("path");
+const socketio = require("socket.io")
+const server = http.createServer(app);
+const io = socketio(server);
+
+
+app.use(express.static(path.join(__dirname,"public")))
+app.get("/",(req,resp)=>{
+    resp.sendFile(path.join(__dirname,"public","index.html"))
+})
+app.get("/about",(req,resp)=>{
+  resp.sendFile(path.join(__dirname,"public","about.html"))
+})
+const userlist =  new Set();
+io.on("connection",(socket)=>{
+    console.log("a user is connected",socket.id);
+    
+    userlist.add(socket.id);
+    socket.emit("myid",socket.id);
+    io.emit("userlist",Array.from(userlist));
+  //outgoing call handling
+   socket.on("outgoingcall", data =>{
+    const {fromoffer,to} = data;
+    
+    socket.to(to).emit("incommingcall",{from:socket.id,offer:fromoffer})
+   })
+  
+   // callaccepted ..
+   socket.on("callaccepted", data =>{
+     const {answer,to} = data;
+     socket.to(to).emit("incomminganswer",{from:socket.id,offer:answer})
+   })
+   
+   
+   socket.on("disconnect",()=>{
+        console.log("user is disconnectedd//")
+        userlist.delete(socket.id);
+        io.emit("userlist",Array.from(userlist))
+    })
+})
+  
+
+server.listen(4000,()=>{
+    console.log("server is running..")
+})
