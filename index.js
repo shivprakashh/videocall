1d3e94615afeb7c1e9b96497866c951c637b1fe2
shiv prakash <123381545+shivprakashh@users.noreply.github.com>
2024-10-02 14:58:57 +0630

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = express();
 const http = require("https")
 const path = require("path");
 const socketio = require("socket.io")
+const cors = require("cors"); 
+app.use(cors());
 
 
 const server = http.createServer(app);
